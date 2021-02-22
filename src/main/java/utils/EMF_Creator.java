ff2d5e48e56a3d088f984ea55bfe0c3c8b39a480
Emil Elkjær Nielsen <emil@visiontrade.dk>
2021-02-22 20:07:16 +0100

    Last adjusts to startcode

--- src/main/java/utils/EMF_Creator.java
+++ src/main/java/utils/EMF_Creator.java
@@ -43,8 +43,8 @@ public class EMF_Creator {
             System.out.println("CONNECTION_STR -->" + System.getenv("CONNECTION_STR"));
             String user = System.getenv("USER");
             String pw = System.getenv("PW");
-//            HUSK at ændre databasenavn herinde
             String connection_str = System.getenv("CONNECTION_STR");
+            connection_str += "ca1";
             Properties props = new Properties();
             props.setProperty("javax.persistence.jdbc.user", user);
             props.setProperty("javax.persistence.jdbc.password", pw);
