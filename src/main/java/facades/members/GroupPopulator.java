4bb4057a634e97d23d1e996ab10c7a6a00522216
Emil Elkjær Nielsen <emil@visiontrade.dk>
2021-02-23 11:41:38 +0100

    Changes to Group Members

---
+++ src/main/java/facades/members/GroupPopulator.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facades.members;
+
+import dtos.members.GroupMemberDTO;
+import entities.members.GroupMember;
+import javax.persistence.EntityManagerFactory;
+import utils.EMF_Creator;
+
+/**
+ * @author Emil
+ */
+public class GroupPopulator {
+    public static boolean populate(){
+        System.out.println("Running " + GroupPopulator.class.getSimpleName());
+        EntityManagerFactory emf = EMF_Creator.createEntityManagerFactory();
+        GroupMemberFacade fe = GroupMemberFacade.getFacadeExample(emf);
+        try{
+        fe.create(new GroupMemberDTO(new GroupMember("Emil Elkjær Nielsen", "cph-en93@cphbusiness.dk", new String[]{"Keeping up with the kardasians", "Matador"})));
+        fe.create(new GroupMemberDTO(new GroupMember("Sigurd Arik Gaarde Nielsen", "cph-at89@cphbusiness.dk", new String[]{"Huset på Christianshavn", "SWAT"})));
+        fe.create(new GroupMemberDTO(new GroupMember("Jacob Lange Nielsen", "cph-jn352@cphbusiness.dk", new String[]{"Ex on the Beach - Norge", "Paradise Hotel"})));
+        return true;
+        } catch (Exception e){
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+        populate();
+    }
+}
